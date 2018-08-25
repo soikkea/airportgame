1b53f6b949373ea9f73a3964de0ef9d9c1678066
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2018-08-25 15:00:27 +0300

    Created a new branch for splines

---
+++ spline_test.py
@@ -0,0 +1,43 @@
+# Testing splines
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+import pygame
+
+from colors import BLACK, RED
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((800, 600))
+
+    points = []
+
+    running = True
+    while running:
+
+        # *** EVENT HANDLING ***
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                running = False
+            if (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                points.append((mouse_x, mouse_y))
+                logger.debug("Clicked x: %d, y: %d", mouse_x, mouse_y)
+        
+        # *** DRAWING ***
+        screen.fill(BLACK)
+        for point in points:
+            pygame.draw.circle(screen, RED, point, 5)
+            #logger.debug("Point drawn at x: %d, y: %d", *point)
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    main()
+    pygame.quit()
