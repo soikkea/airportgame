6d4ed02c0f147bc9d6001905c24babb0effaa27d
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2018-10-22 18:14:46 +0300

    Added unit testing

---
+++ tests/test_airfield.py
@@ -0,0 +1,17 @@
+import unittest
+
+from airfield import Airfield
+
+
+class TestAirfield(unittest.TestCase):
+
+    def test_dist_to_segment(self):
+        field = Airfield()
+        a = (0, 0)
+        b = (0, 10)
+        for c_y in [0, 5, 10]:
+            c = (5, c_y)
+            self.assertAlmostEqual(field.dist_to_segment(a, b, c), 5)
+
+if __name__ == '__main__':
+    unittest.main()
